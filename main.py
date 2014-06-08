6ef5198a5698bc0bc345cce9dea93885195a0335
oohira <oohira.dev@gmail.com>
2014-06-08 21:23:51 +0900

    Tweet current datetime using tweepy.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import datetime
+import tweepy
 import webapp2
+import yaml
 from google.appengine.api import users
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -11,6 +15,25 @@ class MainPage(webapp2.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
 
 
+class DaysLeftBot(webapp2.RequestHandler):
+    def get(self):
+        msg = self.tweet_current_datetime()
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(msg)
+
+    def tweet_current_datetime(self):
+        config = yaml.safe_load(open('config.yaml').read())
+        auth = tweepy.OAuthHandler(config['API_KEY'], config['API_SECRET'])
+        auth.set_access_token(
+            config['ACCESS_TOKEN'], config['ACCESS_TOKEN_SECRET'])
+        api = tweepy.API(auth)
+        now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
+        msg = 'Now {0:%Y-%m-%d %H:%M:%S} JST'.format(now)
+        api.update_status(msg)
+        return msg
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/days-left-bot', DaysLeftBot),
 ], debug=True)
